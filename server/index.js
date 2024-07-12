1cbbc182efce79829ea8b3c5f8dfbd50817c457f
Shaurya Jha <shauryajha35@gmail.com>
2024-07-13 01:42:05 +0530

    add index.js uncomment

--- server/index.js
+++ server/index.js
@@ -1,34 +1,25 @@
-// //express config
-// const express = require('express');
-// const app = express();
-
-// //dotenv config
-// require('dotenv').config();
-
-// //route config
-// const adminRoute = require("./routes/admin");
-// const quizRoute = require("./routes/quiz");
-
-// //use functions 
-// app.use("/api/v1/admin", adminRoute);
-// app.use("/api/v1/quiz", quizRoute);
-
-// //database connection
-// mongoose
-// .connect(process.env.MONGO_URI)
-// .then(() => {
-//     app.listen(process.env.PORT,() => {
-//         console.log(`Connected to DB and running on port https://localhost:${process.env.PORT}`)
-//     })
-// })
-
+//express config
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "yo"
+//dotenv config
+require('dotenv').config();
+
+//route config
+const adminRoute = require("./routes/admin");
+const quizRoute = require("./routes/quiz");
+
+//use functions 
+app.use("/api/v1/admin", adminRoute);
+app.use("/api/v1/quiz", quizRoute);
+
+//database connection
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(process.env.PORT,() => {
+        console.log(`Connected to DB and running on port https://localhost:${process.env.PORT}`)
     })
 })
 
-app.listen(8080);
+
