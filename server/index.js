449f5613c2839fd71cdd752d610f9bf1175f680f
Shaurya Jha <shauryajha35@gmail.com>
2024-07-13 01:36:06 +0530

    remove .env

--- server/index.js
+++ server/index.js
@@ -1,23 +1,34 @@
-//express config
+// //express config
+// const express = require('express');
+// const app = express();
+
+// //dotenv config
+// require('dotenv').config();
+
+// //route config
+// const adminRoute = require("./routes/admin");
+// const quizRoute = require("./routes/quiz");
+
+// //use functions 
+// app.use("/api/v1/admin", adminRoute);
+// app.use("/api/v1/quiz", quizRoute);
+
+// //database connection
+// mongoose
+// .connect(process.env.MONGO_URI)
+// .then(() => {
+//     app.listen(process.env.PORT,() => {
+//         console.log(`Connected to DB and running on port https://localhost:${process.env.PORT}`)
+//     })
+// })
+
 const express = require('express');
 const app = express();
 
-//dotenv config
-require('dotenv').config();
-
-//route config
-const adminRoute = require("./routes/admin");
-const quizRoute = require("./routes/quiz");
-
-//use functions 
-app.use("/api/v1/admin", adminRoute);
-app.use("/api/v1/quiz", quizRoute);
-
-//database connection
-mongoose
-.connect(process.env.MONGO_URI)
-.then(() => {
-    app.listen(process.env.PORT,() => {
-        console.log(`Connected to DB and running on port https://localhost:${process.env.PORT}`)
+app.get("/", (req, res) => {
+    res.json({
+        message: "yo"
     })
 })
+
+app.listen(8080);
